5af37441e25acfc1bc1b05897bde685a1da8d3f8
jawad <jawad@digitify.com>
2020-08-30 00:40:04 +0500

    Initial Commit

---
+++ Switches/Switches.h
@@ -0,0 +1,19 @@
+//
+//  Switches.h
+//  Switches
+//
+//  Created by Jawad Ali on 30/08/2020.
+//  Copyright © 2020 Jawad Ali. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Switches.
+FOUNDATION_EXPORT double SwitchesVersionNumber;
+
+//! Project version string for Switches.
+FOUNDATION_EXPORT const unsigned char SwitchesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Switches/PublicHeader.h>
+
+
